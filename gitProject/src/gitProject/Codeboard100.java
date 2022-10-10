15c31d3732200399a72d44075386c1c70f36e457
Numan <numan35@gmail.com>
2022-10-10 18:02:34 -0400

    codeboard assignment #100

--- gitProject/src/gitProject/Codeboard100.java
+++ gitProject/src/gitProject/Codeboard100.java
@@ -1,9 +1,21 @@
 package gitProject;
 
+import java.util.Scanner;
+
 public class Codeboard100 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			Scanner input = new Scanner(System.in);
+			String[] arr = new String[5];
+			
+			for (int i=0;i<5;i++) {
+				arr[i] = input.nextLine();
+			}
+
+			for (int i = 0; i < arr.length; i++) {
+				System.out.println(arr[i].toString().substring(0,3));
+			}
 
 	}
 
