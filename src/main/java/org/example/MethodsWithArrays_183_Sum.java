b1127aee2cdd19772f81da0bfd2c2922099e2ca3
numan35 <32787732+numan35@users.noreply.github.com>
2022-12-01 21:05:39 -0500

    Commit message

---
+++ src/main/java/org/example/MethodsWithArrays_183_Sum.java
@@ -0,0 +1,28 @@
+package org.example;
+
+/*Instructions from your teacher:
+
+On the left, given an ArrayList of Integers called ints, create and return the sum of all the
+ Integers in ints.
+
+Note: First add values (11, 12, 13, 14, 15) into ints ArrayList. in main method.
+
+At the end pass ints arraylist to sum() method and print its output in console*/
+
+import java.util.ArrayList;
+
+public class MethodsWithArrays_183_Sum {
+
+    public static void main(String[] args) {
+        // Creating object of ArrayList<Integer>
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+        // Add values
+        // ...
+        // ... your code
+    }
+
+    public static int sum(ArrayList<Integer> ints)	{
+        //write code here
+        return 0;
+    }
+}
---
+++ src/test/java/org/example/MethodsWithArrays_183_Sum_TEST.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class MethodsWithArrays_183_Sum_TEST {
+}
